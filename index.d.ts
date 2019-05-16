dc6aa9b628491a74beee3ca4c03d405b3da525ba
Aditya Pratap Singh <adisinghrajput@gmail.com>
2019-05-16 19:26:17 +0200

    feat: Add TS types for rolling window (#3)
    * feat: Add TS types for rolling window
    
    * feat: Fix type for TimeWindow
    
    * feat: Remove ambient context and export class directly
    
    * feat: Create interface for Snapshot type
    
    * feat: Use AbstractHistogram from hdr-hostogram-js
    
    * feat: make input histogram optional

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+import { AbstractHistogram } from "hdr-histogram-js";
+
+type TimeWindow = number; // e.g. 60 * 1000
+
+export interface RollingWindowOptions {
+    numChunks?: number;
+    timeWindow?: TimeWindow;
+    buildHistogram?: () => AbstractHistogram;
+}
+
+export class RollingWindow {
+    constructor(options?: RollingWindowOptions);
+
+    /**
+     * Rotate the rolling window positions
+     */
+    rotate(): void;
+
+    /**
+     * Record value for the current window
+     */
+    recordValue(value: number): void;
+
+    /**
+     * Get the histogram for the current window
+     * @param histogram Optional histogram if provided
+     */
+    getSnapshot(histogram?: AbstractHistogram): AbstractHistogram;
+
+    /**
+     * Start the rolling window
+     */
+    start(): void;
+
+    /**
+     * Stop the rolling window
+     */
+    stop(): void;
+}
